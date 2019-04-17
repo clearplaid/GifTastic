4f2aeb5e058d7041f086a4e97cc15c9cfd6a1bfe
Scott <clearplaid@gmail.com>
2019-04-17 17:44:20 -0400

    Created gif app with still images and dynamic buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-// Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+$(document).ready(function() {
+// you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
 
-// We chose animals for our theme, but you can make a list to your own liking.
 // Your app should take the topics in this array and create buttons in your HTML.
 
 // Try using a loop that appends a button for each string in the array.
@@ -10,16 +10,81 @@
 
 // Under every gif, display its rating (PG, G, so on).
 
-// This data is provided by the GIPHY API.
-// Only once you get images displaying with button presses should you move on to the next step.
 // Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
- 
  
 // maximum of 42 search requests an hour, 1000 search requests a day and 10 upload requests a day 
 // q: string
 // Search query term or phrase.
 
-// GIPHY search will automatically look for exact matches to queries + AND match + OR match. Explicit AND + OR boolean clauses in search queries are not supported.
-// switch the protocol in the query URL from http to https
- var apiKey = "4BxAtLQS1DBCV2uh00Cgo6U8Y4odQHu8";
- var topics = [""]
+    // apiKey = "4BxAtLQS1DBCV2uh00Cgo6U8Y4odQHu8";
+
+    //  Initial topics array
+    var topicsArray = ["Video Games", "Board Games", "Wine", "Anime", "Swimming", "Animals"];
+
+    //  Function for displaying data
+    renderButtons();
+
+    // Add a click event listener to all elements with a class of "topic"
+    $(document).on("click", ".topic", displayTopicGifs);
+        
+    function displayTopicGifs(){
+        var topic = $(this).attr("data-topic");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=4BxAtLQS1DBCV2uh00Cgo6U8Y4odQHu8&limit=10&lang=en";
+
+        // creating AJAX call for the actual button clicked
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+
+        var results = response.data;
+        console.log(results);
+        // Looping through each result item and creating information divs to put the results in the html
+        for (let i = 0; i < results.length; i++) {
+
+            var topicDiv = $("<div class='topicDiv'>");
+            var rating = results[i].rating;   
+            var ratedP = $('<p>').text("Rated: " + rating);
+            var topicImage = $("<img>");
+            topicImage.attr("src", results[i].images.downsized_still.url);
+            topicDiv.append(topicImage);
+            topicDiv.append(ratedP);
+            $("#topic-view").prepend(topicDiv);
+        }
+    });
+}
+    // function to make buttons 
+    function renderButtons(){
+        // Delete buttons before adding so that we don't repeat previous buttons
+        $("#buttons-view").empty();
+
+        //  create buttons from the array of topics above
+        var topicsArrayLength = topicsArray.length;
+        for(let i = 0; i < topicsArrayLength; i++){
+            
+            console.log(topicsArray[i]);
+            var buttonTopic = $("<button>");
+            buttonTopic.addClass("topic")
+            buttonTopic.attr('data-topic', topicsArray[i]);
+            buttonTopic.text(topicsArray[i]);
+            $("#buttons-view").append(buttonTopic);
+            
+        }
+    }
+
+    // event listener to activate ajax call for data
+    $("#add-topic").on("click", function() {
+        // Preventing the buttons default behavior when clicked (which is submitting a form)
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var topicInput = $("#topic-input").val().trim();
+
+        // Adding the topic from the textbox to our array and clear text
+        topicsArray.push(topicInput);
+        $("#topic-input").val("");
+        // Calling renderButtons which handles the processing of our topic array
+        renderButtons();
+
+    });
+
+});
